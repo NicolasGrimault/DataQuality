135f377b7adf023ae3de104ea2894f5e871d797b
Nicolas Grimault <nicolas.g21@live.fr>
2020-01-08 15:02:01 +0100

    Add mean max min std value per month)

--- python.py
+++ python.py
@@ -4,6 +4,16 @@ import pandas as pd
 data = pd.read_csv('Climat.csv')
 # print(data.shape)
 #array = data.values
+print("moyenne")
+print(data.mean())
+print("min")
+print(data.min())
+print("max")
+print(data.max())
+print("Ecart type")
+print(data.std())
+
+
 
 for col in data :    
     plt.plot(data[col].dropna(), label=col)
