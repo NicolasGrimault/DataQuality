5024dfffb5227ee239f3d621feb3c25bcd41171c
Nicolas Grimault <nicolas.g21@live.fr>
2020-01-08 14:14:05 +0100

    Add graph temp with slider

---
+++ slider.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.widgets import Slider
+
+data = pd.read_csv('Climat.csv')
+
+figzoom, axzoom = plt.subplots()
+array = []
+for col in data :
+    array = [*array, *data[col].dropna()]   
+
+plt.plot(array)
+plt.subplots_adjust(left=0.25, bottom=0.25)
+plt.xlabel('Jour')
+plt.ylabel('Température (°C)')
+
+axzoom.set(xlim=(1, 31), autoscale_on=False,title='Zoom window')
+axcolor = 'lightgoldenrodyellow'
+axX = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor=axcolor)
+sX = Slider(axX, 'Freq', 1, 365, valinit=15, valstep=1)
+
+def update(val):
+    index = val
+    min = int(val - 15)
+    max = int(val + 15)
+    if min < 1:
+        min = 1
+    if max > 365:
+        max = 365
+    axzoom.set_xlim(min,max)
+
+
+sX.on_changed(update)
+
+plt.legend()
+plt.show()
